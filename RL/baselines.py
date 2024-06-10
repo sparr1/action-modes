ec495f7b50af6088163dde027ab46f16db520012
Shane Parr <shane_parr@brown.edu>
2024-06-10 19:55:15 -0400

    add baseline configurations for experimentation

--- RL/baselines.py
+++ RL/baselines.py
@@ -5,24 +5,29 @@ module_name = "stable_baselines3" #for dynamic importing
 
 def get_baseline_model(name, env, params = None):
     p = {}
+
     model = None
 
+    #needed to handle gymnasium robotics observed_goal structure properly
+    if "policy" not in params:
+        if type(env.observation_space) == gym.spaces.dict.Dict:
+            p["policy"] = "MultiInputPolicy"
+        else:
+            p["policy"] = "MlpPolicy" #just a guess.
+            print("Trying MlpPolicy as none given.")
+
+    p["env"] = env
+
     if params:
-       p = unwrap_params(params)
+        p.update(params)
 
     if "verbose" not in p.keys():
         p["verbose"] = 1
-    #needed to handle gymnasium robotics observed_goal structure properly
-    if "policy" not in p.keys() and type(env.observation_space) == gym.spaces.dict.Dict:
-        p["policy"] = "MultiInputPolicy"
-    else:
-        p["policy"] = "MlpPolicy" #TODO: check if others
-
     try:
         module = importlib.import_module(module_name)
         model_class = getattr(module,name) #grab the specific algorithm
     
-        model = model_class(p["policy"], env, verbose = p["verbose"])
+        model = model_class(**p) #make sure the json elements actually correspond to parameters of the relevant stable_baseline alg!
         
         return model
         
@@ -30,5 +35,3 @@ def get_baseline_model(name, env, params = None):
         raise ValueError(f"Could not find model class '{name}' in module '{module_name}': {e}")
 
 #TODO handle baseline params
-def unwrap_params(self, params):
-    pass
---
+++ main.py
@@ -0,0 +1,61 @@
+import random, time, argparse, json
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+import gymnasium as gym
+from RL.baselines import get_baseline_model
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="action mode learning experiments")
+    parser.add_argument('-r', '--run', help='config file for a run', required=True)
+    config = vars(parser.parse_args())['run']
+
+    with open(config) as f:
+        experiment_params = json.load(f)
+    print(experiment_params)
+
+
+    alg_list = []
+    num_algs = len(experiment_params["configs"])
+
+    r_eps     = np.zeros((num_algs, experiment_params["trials"], experiment_params['episodes']))
+    #TODO: store obs also
+    time_eps  = np.zeros_like(r_eps)
+    steps_eps = np.zeros_like(r_eps)
+
+    # seed = experiment_params["seed"]
+
+    for i, alg_config in enumerate(experiment_params["configs"]):
+        with open("configs/algs/"+alg_config+".json") as f:
+            run_params = json.load(f)
+        print(run_params)
+        
+        random.seed(experiment_params['seed']) #is it really correct to do this in the loop with the outer experiment seed?
+        np.random.seed(experiment_params['seed'])
+        
+        if(run_params['env'] != experiment_params['env']):
+            raise Exception(f"Mismatch of algorithm and experiment environment configurations: algorithm expects {run_params['env']} while experiment expects {experiment_params['env']}")
+        
+        domain = gym.make(run_params['env'])
+        alg_name = run_params["alg"]
+        if "baselines" in run_params["alg"]: #currently all that is supported. TODO support non-baselines also
+            alg_name = run_params["alg"].split('/')[-1]
+            print(alg_name)
+            model = get_baseline_model(alg_name, domain, params = run_params["alg_params"])
+
+        for t in range(experiment_params["trials"]):
+            # alg_state = {}      
+            steps = 0
+            e = 0
+            all_data = np.zeros((experiment_params["episodes"],))
+
+            model.learn(total_timesteps=run_params["total_steps"])
+
+
+
+
+
+        
+        
