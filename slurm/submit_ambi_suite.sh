e60a1938d6b28da66eb493dcef3a50a0e2fc5612
Richard Gao <rgao.2003@gmail.com>
2026-07-24 11:53:33 -0700

    configs

--- slurm/submit_ambi_suite.sh
+++ slurm/submit_ambi_suite.sh
@@ -7,10 +7,10 @@ cd "$PROJECT_DIR"
 
 launchers=(
   "slurm/run_ambi_anchor.sbatch"
-  "slurm/run_ambi_branch_n16.sbatch"
+  "slurm/run_ambi_horizon_h6.sbatch"
   "slurm/run_ambi_branch_n64.sbatch"
   "slurm/run_ambi_horizon_h1.sbatch"
-  "slurm/run_ambi_horizon_h6.sbatch"
+  "slurm/run_ambi_branch_n16.sbatch"
 )
 
 for launcher in "${launchers[@]}"; do
